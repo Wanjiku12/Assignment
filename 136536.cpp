b4cc19450401baf8739b55589ccf8d34328a86be
Wanjiku12 <109921717+Wanjiku12@users.noreply.github.com>
2022-10-11 14:52:26 +0300

    Add files via upload

---
+++ 136536.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<" O \n";
+    cout<<"-|-"<<endl;
+    cout<<"/";
+    cout<<" \\"<<endl;
+    cout<<" O \n";
+    cout<<"-|-"<<endl;
+    cout<<" * "<<endl;
+    cout<<"***\n";
+    
+    cout<<"/";
+    cout<<" \\"<<endl;
+
+    return 0;
+}
